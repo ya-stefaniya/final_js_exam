3b9ee56550631960ad6e6e3cd11f2410c3c800e1
Stefaniya Yanovich <stefanishy@gmail.com>
2021-01-14 15:14:52 +0300

    burfer fix & behaviour

--- diplom_middle/src/modules/burger.js
+++ diplom_middle/src/modules/burger.js
@@ -1,4 +1,27 @@
 const burgerCreate = ()=>{
-    return 1*10;
+    const popupMenu = document.querySelector('.popup-menu'),
+        burgetBtn = document.querySelector('.menu-button img');
+        
+        burgetBtn.addEventListener('click', () =>{
+            popupMenu.style.display = "flex";
+        })
+
+        popupMenu.addEventListener('click', (e)=>{
+            let target = e.target;
+            if(target.closest('.close-menu-btn') || target.closest('ul li a')){
+                popupMenu.style.display = "none";
+            }
+        })
+
+//fix burger btn while scrolling
+    window.addEventListener("scroll", ()=>{
+        const topMenu = document.querySelector('.top-menu');
+            let scroll = window.pageYOffset;
+            if (scroll > topMenu.offsetTop) {
+                topMenu.style.position = 'fixed';
+            } else {
+                topMenu.style.position = 'relative';
+            }
+    });
 }
 export default burgerCreate;
